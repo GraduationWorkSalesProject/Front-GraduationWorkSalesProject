c0338000a3560ee08b7140eff6b2fb4b47b54491
hyjoong <maplesosser@naver.com>
2022-02-19 17:24:51 +0900

    feat: 검색결과 타입 설정 및 api 타입 수정

--- src/api/product.ts
+++ src/api/product.ts
@@ -1,49 +1,54 @@
-import { ProductResponse } from './../types/goods';
+import { ProductResponse, SearchType } from './../types/goods';
 import { client } from './axios';
-import { Client } from './http'; 
- 
- 
+import { Client } from './http';
+import { AxiosResponse } from 'axios';
+
 export const AddProductApi = async (form: any) => {
-  const response = await Client.post('/products', form, 'multipart/form-data'); 
+  const response = await Client.post('/products', form, 'multipart/form-data');
   return response;
 };
 
-export const loadProductsRecent  = async () =>{
+export const loadProductsRecent = async () => {
   const response = await client.get(`/products/lists/recent`);
   if (response.status !== 200) {
     throw response.status;
   }
-  console.log("res",response);
+  console.log('res', response);
   return response;
-}
+};
 
-export const loadProductsBest  = async () =>{
+export const loadProductsBest = async () => {
   const response = await client.get(`/products/lists/like`);
   if (response.status !== 200) {
     throw response.status;
   }
-  console.log("res",response);
+  console.log('res', response);
   return response;
-}
+};
 
-export const loadProduct = async( id : number ) =>{
+export const loadProduct = async (id: number) => {
   const response = await client.get<ProductResponse>(`/products/${id}`);
-  if(response.status !== 200){
+  if (response.status !== 200) {
     throw response.status;
   }
-  console.log("res", response);
+  console.log('res', response);
   return response.data;
-}
+};
 
-export const searchProduct = async(keyword:string,sort:string, page:number) =>{
-  const response = await client.post(`/products`,{
+export const searchProduct = async (
+  keyword: string,
+  sort: string,
+  page: number,
+) => {
+  const response: AxiosResponse<SearchType> = await client.post(`/products`, {
     keyword,
     sort,
-    page
+    page,
   });
-  if(response.status !== 200){
+
+  if (response.status !== 200) {
     throw response.status;
   }
-  console.log("res", response);
+  console.log('res', response);
   return response.data;
-}
+};
