8f38c0751423209317260ac42ecdcffa5f23b698
hyjoong <maplesosser@naver.com>
2022-02-19 20:12:20 +0900

    feat: list-goods 재 사용 가능하게 수정 - MainPage 베스트 상품들 - MainPage 최근 등록한 상품들 - ProductSearchPage 검색한 상품들

--- src/components/list-goods/List-goods.tsx
+++ src/components/list-goods/List-goods.tsx
@@ -1,46 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Goods from '../goods/Goods';
 import styled from 'styled-components';
-import { loadProductsBest, loadProductsRecent } from '../../api/product';
 import { Link } from 'react-router-dom';
+import { GoodsProps } from '../../types/goods';
 
-const ListGoods = (text: any) => {
-  const [products, setProducts] = useState([]);
+interface IProps {
+  data: GoodsProps[];
+  title: string;
+}
 
-  const loadRecentData = async () => {
-    try {
-      const response = await loadProductsRecent();
-      const temp: any = response.data;
-      setProducts(temp.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const loadBestData = async () => {
-    try {
-      const response = await loadProductsBest();
-      const temp: any = response.data;
-      setProducts(temp.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    if (text.title === 'New ITEMS') {
-      loadRecentData();
-    } else if (text.title === 'Best ITEMS') {
-      loadBestData();
-    }
-  }, []);
-
+const ListGoods = ({ data, title }: IProps) => {
   return (
     <Section>
       <Wrapper>
         <ListTop>
           <TitleWapper>
-            <span>{text.title}</span>
+            <span>{title}</span>
           </TitleWapper>
           <More>
             <button>더보기</button>
@@ -48,7 +23,7 @@ const ListGoods = (text: any) => {
         </ListTop>
         <Divide></Divide>
         <ListBottom>
-          {products.map((item, idx) => (
+          {data.map((item, idx) => (
             <Link to={`products/${item.id}`}>
               <Goods data={item} key={idx}></Goods>
             </Link>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ListGoods from '../components/list-goods/List-goods';
 import Carousel from '../components/Carousel';
 import CategoryMap from '../components/CategoryMap';
+import { loadProductsBest, loadProductsRecent } from '../api/product';
 
 const MainPage = () => {
+  const [recentData, setRecentData] = useState([]);
+  const [bestData, setBestData] = useState([]);
+
+  const loadRecentData = async () => {
+    try {
+      const response = await loadProductsRecent();
+      setRecentData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loadBestData = async () => {
+    try {
+      const response = await loadProductsBest();
+      setBestData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    loadRecentData();
+    loadBestData();
+  }, []);
+
   return (
     <>
-      <Carousel /> 
-      <ListGoods title="Best ITEMS"/>
+      <Carousel />
+      <ListGoods data={bestData} title="Best ITEMS" />
       <CategoryMap />
-      <ListGoods title="New ITEMS"/>
+      <ListGoods data={recentData} title="New ITEMS" />
     </>
   );
 };
