fafaf6fac8d60c6d0fe4bdfbd4c8c34ad1d10d6e
hyjoong <maplesosser@naver.com>
2022-02-22 15:48:19 +0900

    feat: 검색 결과 없을 경우 상품 추천 구현

--- src/components/Search/NoSearchResults.tsx
+++ src/components/Search/NoSearchResults.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
+import useBestItems from '../../hooks/useBestItems';
 import SearchIcon from '../common/icons/SearchIcon';
+import Skeleton from '../common/Skeleton';
+import ListGoods from '../list-goods/List-goods';
 
 const NoSearchResults = ({ search }: { search: string }) => {
+  const { product, loading, responseOK } = useBestItems();
+  const getSkeletonUI = () => (
+    <>
+      <Skeleton type="TEXT" size="MEDIUM" width="14rem" margin="1rem 2rem" />
+    </>
+  );
   return (
-    <NotificationSection>
-      <SearchIcon />
-      <p>{`${search}에 대한 검색 결과가 없습니다.`}</p>
-    </NotificationSection>
+    <>
+      <NotificationSection>
+        <SearchIcon />
+        <p>{`${search}에 대한 검색 결과가 없습니다.`}</p>
+      </NotificationSection>
+      <RecommendSection>
+        {loading ? (
+          getSkeletonUI()
+        ) : (
+          <ListGoods data={product} title="이런 작품은 어때요?" />
+        )}
+      </RecommendSection>
+    </>
   );
 };
 const NotificationSection = styled.section`
-  padding: 7rem 2rem;
+  padding: 8rem 2rem;
 
   display: flex;
   flex-direction: column;
@@ -24,5 +42,9 @@ const NotificationSection = styled.section`
     text-align: center;
     font-weight: 700;
   }
+`;
+
+const RecommendSection = styled.section`
+  padding: 1rem 2rem;
 `;
 export default NoSearchResults;
